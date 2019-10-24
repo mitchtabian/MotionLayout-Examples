8a9198a86be68038fe20ffe0e7561719e80dba37
Mitch Tabian <mitch@tabian.ca>
2019-10-24 14:23:54 -0700

    Collapsing Toolbar with animation

--- app/src/main/java/com/codingwithmitch/motionlayoutexample/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/motionlayoutexample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.codingwithmitch.motionlayoutexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        coordinateMotion()
+    }
+
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
     }
 }
 
